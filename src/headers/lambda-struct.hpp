259712ba0c6207e86bdfed3966791a079d1d9fa8
Benedikt Franke <benedikt.franke@uni-ulm.de>
2020-07-27 19:37:08 +0200

    began incorporating feedback; to_string replaced by operator<<; equality operators removed

--- src/headers/lambda-struct.hpp
+++ src/headers/lambda-struct.hpp
@@ -3,19 +3,9 @@
 #include <sstream>
 #include "lambda-exceptions.hpp"
 
-#ifndef MAX_ITER
-#define MAX_ITER 1000
-#endif
-
 // TODO: methods for alpha equivalence
-// TODO: alpha_convert does not check if the new name already exists
 // TODO: invoke alpha_convert if beta reduction would lead to name clashes
 // TODO: "(\ x . (x) x) \ y. (y) y" terminates after one reduction, while it should lead to an infinite loop
-
-typedef unsigned long ID;
-
-// counter for unique ids for variables
-static ID _next_id = 0;
 
 // forward declarations
 class Expression;
@@ -27,127 +17,26 @@ typedef std::shared_ptr<Application> Application_ptr;
 typedef std::shared_ptr<Variable> Variable_ptr;
 typedef std::shared_ptr<Lambda> Lambda_ptr;
 
-class Expression {
+  class Expression: public std::enable_shared_from_this<Expression> {
     /**
      * abstract base class for all valid expressions
      */
   public:
-    Expression() = delete;
-    virtual std::string to_string() const noexcept = 0;
-    std::string get_name() const {
-        return name;
-    }
-    virtual bool check_for_name_clash(const std::string& new_name) const noexcept = 0;
+    /** @return true iff the expression contains a variable with a name equal to the given string */
+    virtual bool check_for_name_clash(const std::string&) const noexcept = 0;
+
+    /** @return Expression after one step of beta reduction*/
     virtual Expression_ptr beta_reduce() = 0;
-    virtual Expression_ptr bind(Variable_ptr, Expression_ptr) = 0;
-    virtual Expression_ptr alpha_convert(const Variable&, const std::string&, bool=true) = 0;
-    virtual Variable_ptr get_head() = 0;
-    // literal equivalence
-    virtual bool operator==(const Expression& other) const noexcept = 0;
-    bool operator!=(const Expression& other) const noexcept {
-        return !(*this == other);
-    }
+
+    /** @return Expression where Variable_ptr was replaced by Expression_ptr*/
+    virtual Expression_ptr substitute(Variable_ptr, Expression_ptr) = 0;
+
+    /** @return Expression where bound variables with a name equal to the first argument were renamed to teh second
+     * if third argument is true, throws Exception on name clash */
+    virtual Expression_ptr alpha_convert(const std::string&, const std::string&, bool=true) = 0;
     // virtual bool alpha_equals(const Expression& other) const noexcept = 0;
-    Expression_ptr beta_reduce_full() {
-        Expression_ptr reduced;
-        Expression_ptr expr = this->beta_reduce();
-        int i = 0;
-        do {
-            if(i > 0) expr = reduced;
-            reduced = expr->beta_reduce();
-        } while((*reduced) != (*expr) && ++i < MAX_ITER);
-        if(i == MAX_ITER) {
-            throw MaxIterationsExceeded();
-        }
-        return reduced;
-    }
-
   protected:
-    Expression(std::string name) : name(name) {}
-    std::string name;
-};
-
-class Application final : public Expression {
-    /**
-     * two Expressions after each other
-     * e.g. x y or (\ x . x) y
-     */
-  public:
-    Application(std::string name) : Expression(name), fst(), snd() {}
-    Application(std::string name, Expression_ptr fst, Expression_ptr snd) : Expression(name), fst(fst), snd(snd) {}
-
-    Expression_ptr bind(Variable_ptr e_old, Expression_ptr e_new) override {
-        /**
-         * binds e_old to e_new in both expressions of this application
-         * returns new Expression
-         */
-        return std::make_shared<Application>(name, fst->bind(e_old, e_new),
-                snd->bind(e_old, e_new));
-    }
-    Expression_ptr beta_reduce() override {
-        /**
-         * invokes a beta reduction:
-         * if fst is a lambda, binds second to the bound variable in fst
-         * otherwise, passes beta-reduction on to fst and snd
-         */
-        // if it has a head, it is a lambda
-        if(auto h = fst->get_head(); h) return fst->bind(h, snd);
-        auto res1 = std::make_shared<Application>(name, fst->beta_reduce(), snd);
-        if(*(res1->fst) == *fst) {
-            // no changes, i.e. fst is in normal form
-            return std::make_shared<Application>(name, fst, snd->beta_reduce());
-        }
-        else return res1;
-    }
-    Expression_ptr alpha_convert(const Variable& old, const std::string& new_name, bool check_clashes=true) override {
-        /**
-         * renames bound variable old_name to new_name
-         */
-        if(check_clashes) {
-            if(this->check_for_name_clash(new_name)) throw NameClash();
-        }
-        Expression_ptr res = std::make_shared<Application>(name, fst->alpha_convert(old, new_name),
-                snd->alpha_convert(old, new_name), false);
-        return res;
-    }
-    std::string to_string() const noexcept override {
-        /**
-         * string representation
-         */
-        std::stringstream ss;
-        auto fst_str = fst->to_string();
-        auto snd_str = snd->to_string();
-        if(isspace(fst_str.back())) fst_str.pop_back();
-        //ss << "( " << fst->to_string() << " " << snd->to_string() << ") ";
-        if(fst->get_head() != nullptr) ss << "(" << fst_str << ")" << " ";
-        else ss << "(" << fst_str << ") ";
-        //if(snd->get_head() != nullptr) ss << "(" << snd_str << ")" << " ";
-        //else
-        ss << snd_str;
-        return ss.str();
-    }
-    Variable_ptr get_head() override {
-        /**
-         * returns nullptr, as only lambdas have a head
-         */
-        return std::shared_ptr<Variable>(nullptr);
-    }
-    bool operator==(const Expression& other) const noexcept override {
-        /**
-         * tests if both expressions are literally equal, i.e. same structure and all variable names must match
-         */
-        // taken from https://stackoverflow.com/questions/1765122/equality-test-for-derived-classes-in-c
-        if(typeid(*this) != typeid(other)) return false;
-        auto o = static_cast<const Application&>(other);
-        return *fst == *(o.fst) && *(snd) == *(o.snd);
-    }
-    bool check_for_name_clash(const std::string& new_name) const noexcept {
-        return fst->check_for_name_clash(new_name) || snd->check_for_name_clash(new_name);
-    }
-  private:
-    Expression_ptr fst;
-    Expression_ptr snd;
-
+    Expression() {}
 };
 
 class Variable final : public Expression {
@@ -156,31 +45,21 @@ class Variable final : public Expression {
      * e.g. x
      */
   public:
-    Variable() = delete;
-    Variable(std::string name, bool bound) : Expression(name), bound(bound), id(_next_id++) {}
-    Variable(std::string name, bool bound, ID id) : Expression(name), bound(bound), id(id) {}
+    Variable(std::string name, bool bound) : Expression(), name(name), bound(bound) {}
     bool is_bound() const noexcept {
         return bound;
     }
-    ID get_id() const noexcept {
-        /**
-         * id uniquely identifies a variable
-         */
-        return id;
-    }
-    std::string to_string() const noexcept override {
-        /**
-         * string representation of variable is its name
-         */
-        return name + " ";
+    std::string get_name() const noexcept {
+        return name;
     }
     Expression_ptr beta_reduce() override {
         /**
          * beta reduction of variable is identity
          */
-        return std::make_shared<Variable>(name, bound, id);
+        return shared_from_this();
     }
-    Expression_ptr alpha_convert(const Variable& old, const std::string& new_name, bool check_clashes=true) override {
+    Expression_ptr alpha_convert(const std::string& old_name, const std::string& new_name,
+                                 bool check_clashes=true) override {
         /**
          * renames if name matches and variable is bound
          */
@@ -188,38 +67,26 @@ class Variable final : public Expression {
             if(this->check_for_name_clash(new_name)) throw NameClash();
         }
         std::string name;
-        if(bound && id == old.id) name = new_name;
-        else name = this->name;
-        return std::make_shared<Variable>(name, bound, id);
-
+        if(bound && name.compare(old_name) == 0) {
+            name = new_name;
+            return std::make_shared<Variable>(name, bound);
+        }
+        return shared_from_this();
     }
-    Expression_ptr bind(Variable_ptr e1, Expression_ptr e2) override {
+    Expression_ptr substitute(Variable_ptr e1, Expression_ptr e2) override {
         /**
          * returns e2 if e1 matches itself, else returns copy of itself
          */
-        if(id == e1->id) return e2;
-        else return std::make_shared<Variable>(name, bound, id);
-    }
-    Variable_ptr get_head() override {
-        /**
-         * Variable does not have head, returns nullptr
-         */
-        return std::shared_ptr<Variable>(nullptr);
-    }
-    bool operator==(const Expression& other) const noexcept override {
-        /**
-         * tests if both variables are literally equal, i.e. same id and bound-status
-         */
-        if(typeid(*this) != typeid(other)) return false;
-        auto o = static_cast<const Variable&>(other);
-        return bound == o.bound && id == o.id;
+        if(name.compare(e1->name) == 0) return e2;
+        return shared_from_this();
     }
     bool check_for_name_clash(const std::string& new_name) const noexcept {
         return this->name.compare(new_name) == 0;
     }
+
   private:
     bool bound;
-    ID id;
+    std::string name;
 };
 
 class Lambda final : public Expression {
@@ -229,62 +96,51 @@ class Lambda final : public Expression {
      * \ head . body
      */
   public:
-    Lambda() = delete;
-    Lambda(std::string name, Variable_ptr head, Expression_ptr body) : head(head), body(body), Expression(name) {}
-    std::string to_string() const noexcept override {
-        /**
-         * string representation with brackets and "\\ x ." as lambda abstraction
-         */
-        std::stringstream ss;
-        ss << "\\" << head->to_string() << ". " << body->to_string();
-        return ss.str();
-    }
+    Lambda(Variable_ptr head, Expression_ptr body) : head(head), body(body) {}
     Expression_ptr beta_reduce() override {
         /**
          * beta reduction needs an application, so it gets passed down to body
          */
-        return std::make_shared<Lambda>(name, head, body->beta_reduce());
+        auto res = body->beta_reduce();
+        if(res == body) return shared_from_this();
+        return std::make_shared<Lambda>(head, res);
     }
-    Expression_ptr bind(Variable_ptr e1, Expression_ptr e2) override {
+    Expression_ptr substitute(Variable_ptr e1, Expression_ptr e2) override {
         /**
          * if e1 matches head, binds head and returns body
          * else passes binding on to body
          */
-        if(e1->get_id() == head->get_id()) return body->bind(e1, e2);
-        return std::make_shared<Lambda>(name, head, body->bind(e1, e2));
+        if(e1 == head) return body->substitute(e1, e2);
+        auto res = body->substitute(e1, e2);
+        if(res == body) return shared_from_this();
+        return std::make_shared<Lambda>(head, res);
     }
-    Expression_ptr alpha_convert(const Variable& old, const std::string& new_name, bool check_clashes=true) override {
+    Expression_ptr alpha_convert(const std::string& old_name, const std::string& new_name,
+                                 bool check_clashes=true) override {
         /**
          * passes conversion on to head and body
          */
         if(check_clashes) {
             if(this->check_for_name_clash(new_name)) throw NameClash();
         }
-        return std::make_shared<Lambda>(name,
-                std::static_pointer_cast<Variable>(head->alpha_convert(old, new_name)),
-                body->alpha_convert(old, new_name), false);
+        auto res1 = head->alpha_convert(old_name, new_name);
+        auto res2 = body->alpha_convert(old_name, new_name);
+        if(res1 == head && res2 == body) return shared_from_this();
+        return std::make_shared<Lambda>(std::static_pointer_cast<Variable>(res1), res2);
     }
-    Variable_ptr get_head() override {
+    Variable_ptr get_head() noexcept {
         /**
          * returns head, the variable in the abstraction
          * e.g. \ x . x y -> x is head
          */
         return head;
     }
-    Expression_ptr get_body() {
+    Expression_ptr get_body() noexcept {
         /**
          * returns body, the expression after the abstraction
          * e.g. \ x . x y -> x y is body
          */
         return body;
-    }
-    bool operator==(const Expression& other) const noexcept override {
-        /**
-         * tests if both lambdas are literally equal, i.e. head and body have to match
-         */
-        if(typeid(*this) != typeid(other)) return false;
-        auto o = static_cast<const Lambda&>(other);
-        return *head == *(o.head) && *body == *(o.body);
     }
     bool check_for_name_clash(const std::string& new_name) const noexcept {
         return head->check_for_name_clash(new_name) || body->check_for_name_clash(new_name);
@@ -293,3 +149,85 @@ class Lambda final : public Expression {
     Variable_ptr head;
     Expression_ptr body;
 };
+
+
+class Application final : public Expression {
+    /**
+     * two Expressions after each other
+     * e.g. x y or (\ x . x) y
+     */
+  public:
+    Application(Expression_ptr fst, Expression_ptr snd) : Expression(), fst(fst), snd(snd) {}
+
+    Expression_ptr substitute(Variable_ptr e_old, Expression_ptr e_new) override {
+        /**
+         * binds e_old to e_new in both expressions of this application
+         * returns new Expression
+         */
+
+        auto fst_new = fst->substitute(e_old, e_new);
+        auto snd_new = snd->substitute(e_old, e_new);
+        if(fst_new == fst && snd_new == snd) return shared_from_this();
+        return std::make_shared<Application>(fst_new, snd_new);
+    }
+    Expression_ptr beta_reduce() override {
+        /**
+         * invokes a beta reduction:
+         * if fst is a lambda, binds second to the bound variable in fst
+         * otherwise, passes beta-reduction on to fst and snd
+         */
+        if(Lambda_ptr lbd = std::dynamic_pointer_cast<Lambda>(fst); lbd)
+            return lbd->substitute(lbd->get_head(), snd);
+
+        auto res1 = fst->beta_reduce();
+        if(res1 == fst) {
+            // no changes, i.e. fst is in normal form
+            auto res2 = snd->beta_reduce();
+            if(res2 == snd) return shared_from_this();
+            return std::make_shared<Application>(fst, snd->beta_reduce());
+        }
+        return std::make_shared<Application>(res1, snd);
+    }
+    Expression_ptr alpha_convert(const std::string& old_name, const std::string& new_name,
+            bool check_clashes=true) override {
+        /**
+         * renames bound variable old_name to new_name
+         */
+        if(check_clashes) {
+            if(this->check_for_name_clash(new_name)) throw NameClash();
+        }
+        auto res1 = fst->alpha_convert(old_name, new_name);
+        auto res2 = snd->alpha_convert(old_name, new_name);
+        if(res1 == fst && res2 == snd) return shared_from_this();
+        return std::make_shared<Application>(res1, res2);
+
+    }
+    bool check_for_name_clash(const std::string& new_name) const noexcept {
+        return fst->check_for_name_clash(new_name) || snd->check_for_name_clash(new_name);
+    }
+    Expression_ptr get_first() noexcept {
+        return fst;
+    }
+    Expression_ptr get_second() noexcept {
+        return snd;
+    }
+  private:
+    Expression_ptr fst;
+    Expression_ptr snd;
+};
+
+std::ostream& operator<<(std::ostream& os, const Expression& ex) {
+    if(typeid(ex) == typeid(Variable)) {
+        auto v = static_cast<const Variable&>(ex);
+        os << v.get_name();
+    }
+    else if(typeid(ex) == typeid(Lambda)) {
+        auto l = static_cast<const Lambda&>(ex);
+        os << '\\' << *l.get_head() << " . " << *l.get_body();
+    }
+    else if(typeid(ex) == typeid(Application)) {
+        auto a = static_cast<const Application&>(ex);
+        os << "(" << *a.get_first() << ") " << *a.get_second();
+    }
+    return os;
+}
--- test/lambda-struct.cpp
+++ test/lambda-struct.cpp
@@ -15,120 +15,141 @@ TEST(BETA, simple_1) {
     // tests reduction of ( \x . x) hallo
     Variable_ptr v1 = make_shared<Variable>("hallo", false);
     Variable_ptr v2 = make_shared<Variable>("x", true);
-    Lambda_ptr l = make_shared<Lambda>("my lambda", v2, v2);
-    Application_ptr a = make_shared<Application>("outer scope", l, v1);
-    auto res = dynamic_pointer_cast<Variable>(a->beta_reduce());
-    ASSERT_EQ(res->to_string(), "hallo ");
+    Lambda_ptr l = make_shared<Lambda>(v2, v2);
+    Application_ptr a = make_shared<Application>(l, v1);
+    auto res = a->beta_reduce();
+    stringstream ss;
+    ss << *res;
+    ASSERT_EQ(ss.str(), "hallo");
 }
+
 TEST(BETA, simple_2) {
     // tests reduction of (\ x . (\ y . y a) b) c, which requires 2 steps
     auto bounds = make_vars({"x", "y"}, true);
     auto unbounds = make_vars({"a", "b", "c"}, false);
-    Application_ptr a1 = make_shared<Application>("y a", bounds[1], unbounds[0]);
-    Lambda_ptr l1 = make_shared<Lambda>("\\y.ya", bounds[1], a1);
-    Application_ptr a2 = make_shared<Application>("(\\y.ya)b", l1, unbounds[1]);
-    Lambda_ptr l2 = make_shared<Lambda>("\\x.(\\y.ya)b)", bounds[0], a2);
-    Application_ptr out = make_shared<Application>("outer", l2, unbounds[2]);
+    Application_ptr a1 = make_shared<Application>(bounds[1], unbounds[0]);
+    Lambda_ptr l1 = make_shared<Lambda>(bounds[1], a1);
+    Application_ptr a2 = make_shared<Application>(l1, unbounds[1]);
+    Lambda_ptr l2 = make_shared<Lambda>(bounds[0], a2);
+    Application_ptr out = make_shared<Application>(l2, unbounds[2]);
     // first reduction
     auto res1 = out->beta_reduce();
-    ASSERT_EQ(res1->to_string(), "(\\y . (y) a) b ");
+    stringstream ss;
+    ss << *res1;
+    ASSERT_EQ(ss.str(), "(\\y . (y) a) b");
+    ss.str("");
     // second reduction
     auto res2 = res1->beta_reduce();
-    ASSERT_EQ(res2->to_string(), "(b) a ");
+    ss << *res2;
+    ASSERT_EQ(ss.str(), "(b) a");
 }
+
 TEST(ALPHA, simple_1) {
     // lets rename the outer x in \ x . (\ x . x) x
     auto bounds = make_vars({"x", "x"}, true);
-    auto l1 = make_shared<Lambda>("", bounds[0], bounds[0]);
-    auto a1 = make_shared<Application>("", l1, bounds[1]);
-    auto l2 = make_shared<Lambda>("", bounds[1], a1);
-    auto res = l2->alpha_convert(*bounds[1], "y");
-    ASSERT_EQ(res->to_string(), "\\y . (\\x . x) y ");
+    auto l1 = make_shared<Lambda>(bounds[0], bounds[0]);
+    auto a1 = make_shared<Application>(l1, bounds[1]);
+    auto l2 = make_shared<Lambda>(bounds[1], a1);
+    auto res = l2->alpha_convert("x", "y");
+    stringstream ss;
+    ss << *res;
+    ASSERT_EQ(ss.str(), "\\y . (\\x . x) y");
 }
+
 TEST(BETA, conflicting_names) {
     // (\ x . (\ x . \ y . s u x x y ) c e x x ) s
     auto bound = make_vars({"x", "x", "y"}, true);
     auto unbound = make_vars({"s", "u", "c", "e", "s"}, false);
-    auto a1 = make_shared<Application>("", unbound[0], unbound[1]);
-    auto a2 = make_shared<Application>("", a1, bound[1]);
-    auto a3 = make_shared<Application>("", a2, bound[1]);
-    auto a4 = make_shared<Application>("", a3, bound[2]);
-    auto l1 = make_shared<Lambda>("", bound[2], a4);
-    auto l2 =  make_shared<Lambda>("", bound[1], l1);
-    auto a5 = make_shared<Application>("", l2, unbound[2]);
-    auto a6 = make_shared<Application>("", a5, unbound[3]);
-    auto a7 = make_shared<Application>("", a6, bound[0]);
-    auto a8 = make_shared<Application>("", a7, bound[0]);
-    auto l3 = make_shared<Lambda>("", bound[0], a8);
-    auto out = make_shared<Application>("", l3, unbound[4]);
+    auto a1 = make_shared<Application>(unbound[0], unbound[1]);
+    auto a2 = make_shared<Application>(a1, bound[1]);
+    auto a3 = make_shared<Application>(a2, bound[1]);
+    auto a4 = make_shared<Application>(a3, bound[2]);
+    auto l1 = make_shared<Lambda>(bound[2], a4);
+    auto l2 =  make_shared<Lambda>(bound[1], l1);
+    auto a5 = make_shared<Application>(l2, unbound[2]);
+    auto a6 = make_shared<Application>(a5, unbound[3]);
+    auto a7 = make_shared<Application>(a6, bound[0]);
+    auto a8 = make_shared<Application>(a7, bound[0]);
+    auto l3 = make_shared<Lambda>(bound[0], a8);
+    auto out = make_shared<Application>(l3, unbound[4]);
 
 
     auto res1 = out->beta_reduce();
     auto res2 = res1->beta_reduce();
     auto res3 = res2->beta_reduce();
-    ASSERT_EQ(res3->to_string(), "((((((s) u) c) c) e) s) s ");
+    stringstream ss;
+    ss << *res3;
+    ASSERT_EQ(ss.str(), "((((((s) u) c) c) e) s) s");
 }
 TEST(ALPHA, conflicting_names) {
     // (\ x . (\ x . \ y . s u x x y ) c e x x ) s
     // this time with alpha
     auto bound = make_vars({"x", "x", "y"}, true);
     auto unbound = make_vars({"s", "u", "c", "e", "s"}, false);
-    auto a1 = make_shared<Application>("", unbound[0], unbound[1]);
-    auto a2 = make_shared<Application>("", a1, bound[1]);
-    auto a3 = make_shared<Application>("", a2, bound[1]);
-    auto a4 = make_shared<Application>("", a3, bound[2]);
-    auto l1 = make_shared<Lambda>("", bound[2], a4);
-    auto l2 =  make_shared<Lambda>("", bound[1], l1);
-    auto a5 = make_shared<Application>("", l2, unbound[2]);
-    auto a6 = make_shared<Application>("", a5, unbound[3]);
-    auto a7 = make_shared<Application>("", a6, bound[0]);
-    auto a8 = make_shared<Application>("", a7, bound[0]);
-    auto l3 = make_shared<Lambda>("", bound[0], a8);
-    auto out = make_shared<Application>("", l3, unbound[4]);
+    auto a1 = make_shared<Application>(unbound[0], unbound[1]);
+    auto a2 = make_shared<Application>(a1, bound[1]);
+    auto a3 = make_shared<Application>(a2, bound[1]);
+    auto a4 = make_shared<Application>(a3, bound[2]);
+    auto l1 = make_shared<Lambda>(bound[2], a4);
+    auto l2 =  make_shared<Lambda>(bound[1], l1);
+    auto a5 = make_shared<Application>(l2, unbound[2]);
+    auto a6 = make_shared<Application>(a5, unbound[3]);
+    auto a7 = make_shared<Application>(a6, bound[0]);
+    auto a8 = make_shared<Application>(a7, bound[0]);
+    auto l3 = make_shared<Lambda>(bound[0], a8);
+    auto out = make_shared<Application>(l3, unbound[4]);
 
-    auto res = out->alpha_convert(*bound[1], "y")->alpha_convert(*bound[2], "z");
-    ASSERT_EQ(res->to_string(), "(\\x . ((((\\y . \\z . ((((s) u) y) y) z) c) e) x) x) s ");
+    auto res = out->alpha_convert(bound[1]->get_name(), "y")->alpha_convert(bound[2]->get_name(), "z");
+    stringstream ss;
+    ss << *res;
+    ASSERT_EQ(ss.str(), "(\\x . ((((\\y . \\z . ((((s) u) y) y) z) c) e) x) x) s");
 }
 TEST(BETA, first_part_no_reduction) {
     // (g) ((\ x . (x) x) o) d
     auto bound = make_vars({"x"}, true);
     auto unbound = make_vars({"g", "o", "d"}, false);
-    auto a1 = make_shared<Application>("x x", bound[0], bound[0]);
-    auto l1 = make_shared<Lambda>("\\ x . x x", bound[0], a1);
-    auto a2 = make_shared<Application>("app. o", l1, unbound[1]);
-    auto a3 = make_shared<Application>("app. d", a2, unbound[2]);
-    auto a4 = make_shared<Application>("outer", unbound[0], a3);
+    auto a1 = make_shared<Application>(bound[0], bound[0]);
+    auto l1 = make_shared<Lambda>(bound[0], a1);
+    auto a2 = make_shared<Application>(l1, unbound[1]);
+    auto a3 = make_shared<Application>(a2, unbound[2]);
+    auto a4 = make_shared<Application>(unbound[0], a3);
 
     auto res = a4->beta_reduce();
-    ASSERT_EQ(res->to_string(), "(g) ((o) o) d ");
+    stringstream ss;
+    ss << *res;
+    ASSERT_EQ(ss.str(), "(g) ((o) o) d");
 }
 TEST(BETA, NORMAL_ORDER) {
     // from http://www.mathematik.uni-ulm.de/numerik/cpp/ss18/cpp-2018-06-19.pdf
     // (\x . a) (\x . (x) x) \y.(y) y
     auto unbound = make_vars({"a"}, false);
     auto bound = make_vars({"x", "x", "y"}, true);
-    auto a1 = make_shared<Application>("y y", bound[2], bound[2]);
-    auto l1 = make_shared<Lambda>("\\y", bound[2], a1);
-    auto a2 = make_shared<Application>("x x", bound[1], bound[1]);
-    auto l2 = make_shared<Lambda>("\\x...", bound[1], a2);
-    auto a3 = make_shared<Application>("(\\ x) (\\ y)", l2, l1);
-    auto l3 = make_shared<Lambda>("\\x", bound[0], unbound[0]);
-    auto a4 = make_shared<Application>("outer", l3, a3);
+    auto a1 = make_shared<Application>(bound[2], bound[2]);
+    auto l1 = make_shared<Lambda>(bound[2], a1);
+    auto a2 = make_shared<Application>(bound[1], bound[1]);
+    auto l2 = make_shared<Lambda>(bound[1], a2);
+    auto a3 = make_shared<Application>(l2, l1);
+    auto l3 = make_shared<Lambda>(bound[0], unbound[0]);
+    auto a4 = make_shared<Application>(l3, a3);
 
     auto res = a4->beta_reduce();
-    ASSERT_EQ("a ", res->to_string());
+    stringstream ss;
+    ss << *res;
+    ASSERT_EQ(ss.str(), "a");
 }
 TEST(STRING_REPR, test1) {
     // (\x . a) (\x . (x) x) \y.(y) y
     auto unbound = make_vars({"a"}, false);
     auto bound = make_vars({"x", "x", "y"}, true);
-    auto a1 = make_shared<Application>("y y", bound[2], bound[2]);
-    auto l1 = make_shared<Lambda>("\\y", bound[2], a1);
-    auto a2 = make_shared<Application>("x x", bound[1], bound[1]);
-    auto l2 = make_shared<Lambda>("\\x...", bound[1], a2);
-    auto a3 = make_shared<Application>("(\\ x) (\\ y)", l2, l1);
-    auto l3 = make_shared<Lambda>("\\x", bound[0], unbound[0]);
-    auto a4 = make_shared<Application>("outer", l3, a3);
+    auto a1 = make_shared<Application>(bound[2], bound[2]);
+    auto l1 = make_shared<Lambda>(bound[2], a1);
+    auto a2 = make_shared<Application>(bound[1], bound[1]);
+    auto l2 = make_shared<Lambda>(bound[1], a2);
+    auto a3 = make_shared<Application>(l2, l1);
+    auto l3 = make_shared<Lambda>(bound[0], unbound[0]);
+    auto a4 = make_shared<Application>(l3, a3);
 
-    ASSERT_EQ(a4->to_string(), "(\\x . a) (\\x . (x) x) \\y . (y) y ");
+    stringstream ss;
+    ss << *a4;
+    ASSERT_EQ(ss.str(), "(\\x . a) (\\x . (x) x) \\y . (y) y");
 }
