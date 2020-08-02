6eac4fdf4d992dcaa41263b86ca5a039eea322f7
Benedikt Franke <benedikt.franke@uni-ulm.de>
2020-08-02 12:05:18 +0200

    parser now builds syntax tree, church encoding for positive integers

--- src/headers/church-encoding.hpp
+++ src/headers/church-encoding.hpp
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "lambda-struct.hpp"
+
+Lambda_ptr church_encode(unsigned int n) {
+    Variable_ptr f = std::make_shared<Variable>("f", true);
+    Variable_ptr x = std::make_shared<Variable>("x", true);
+    Expression_ptr bdy = x;
+    for(int i = 0; i < n; ++i) bdy = std::make_shared<Application>(f, bdy);
+    auto tmp = std::make_shared<Lambda>(x, bdy);
+    return std::make_shared<Lambda>(f, tmp);
+}
--- src/headers/lambda-syntax.hpp
+++ src/headers/lambda-syntax.hpp
@@ -7,33 +7,37 @@
 
 // TODO: TEST
 // TODO: build the syntax tree while parsing
+// TODO conversion hierarchy
 
 // recursive descent parser inspired by https://en.wikipedia.org/wiki/Recursive_descent_parser
 // and https://www.geeksforgeeks.org/recursive-descent-parser/
-// I built a LL(2) grammar for this, hence we need two lokaheads
+// I built a LL(2) grammar for this, hence we need two lookaheads
 class Parser {
   public:
-    Parser(std::istream& in) : tz(in) {}
+    Parser(std::istream& in) : tz(in), bound() {}
     void program() {
         la1 = tz.get();
         la2 = tz.get();
         if(la1.tok == IDENTIFIER && la2.tok == ASSIGNMENT) {
-            assignment();
+            res = assignment();
         }
         else {
-            rvalue();
+            res = rvalue();
         }
         if(la1.tok != SEPARATOR) throw SyntaxException();
     }
-    void assignment() {
+    Expression_ptr assignment() {
         if(la1.tok != IDENTIFIER) throw SyntaxException();
+        std::string name = la1.str;
         next_token();
         if(la1.tok != ASSIGNMENT) throw SyntaxException();
         next_token();
-        rvalue();
+        Expression_ptr e = rvalue();
+        known_symbols[name] = res;
+        return e;
     }
-    void rvalue() {
-        expression();
+    Expression_ptr rvalue() {
+        Expression_ptr e = expression();
         if(la1.tok == CONV_START) {
             if(la2.tok == IDENTIFIER) {
                 alpha();
@@ -42,27 +46,40 @@ class Parser {
                 beta();
             }
         }
+        return e;
     }
-    void expression() {
+    Expression_ptr expression() {
         if(la1.tok == LAMBDA) {
+            // backup the bound-map, because there could already be a variable with the same name as the new head
+            std::unordered_map<std::string, Variable_ptr> backup_bound = bound;
             next_token();
             if(la1.tok != IDENTIFIER) throw SyntaxException();
+            // build head variable
+            Variable_ptr head = std::make_shared<Variable>(la1.str, true);
+            bound[la1.str] = head;
             next_token();
             if(la1.tok != BODY_START) throw SyntaxException();
             next_token();
-            expression();
+            Expression_ptr body = expression();
+            bound = backup_bound;
+            return std::make_shared<Lambda>(head, body);
         }
         else if(la1.tok == BRACKET_OPEN) {
             next_token();
-            expression();
+            Expression_ptr fst = expression();
             if(la1.tok != BRACKET_CLOSE) throw SyntaxException();
             next_token();
-            expression();
+            Expression_ptr snd = expression();
+            return std::make_shared<Application>(fst, snd);
         }
         else if(la1.tok == IDENTIFIER) {
-            // TODO: do something with the variable
+            if(auto v = bound.find(la1.str); v != bound.end()) {
+                return v->second;
+            }
+            return std::make_shared<Variable>(la1.str, false);
         }
         else if(la1.tok == LITERAL) {
+            throw std::runtime_error("Not implemented yet");
             // TODO: do something with the literal
         }
         else throw SyntaxException();
@@ -73,6 +90,7 @@ class Parser {
             if(la1.tok != t) throw SyntaxException();
             next_token();
         }
+
     }
     void beta() {
         if(la1.tok == CONV_END) {
@@ -104,5 +122,8 @@ class Parser {
     Token la2;
     Tokenizer tz;
     Expression_ptr res;
+    std::unordered_map<std::string, Variable_ptr> bound;
+    std::unordered_map<std::string, Expression_ptr> known_symbols;
+    Token command;
 };
 
